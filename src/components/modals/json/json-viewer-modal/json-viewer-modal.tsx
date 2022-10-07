be245e5dbe3e6ab907831cc3a3450dcacc5c649d
devdave25 <me@dja.dev>
2022-10-07 20:22:57 +0100

    feat(json): add react-ace code viewer

--- src/components/modals/json/json-viewer-modal/json-viewer-modal.tsx
+++ src/components/modals/json/json-viewer-modal/json-viewer-modal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "../../../button/button";
 import { Modal } from "../../modal";
+import AceEditor from "react-ace";
 
 import styles from "./json-viewer-modal.module.scss";
 
@@ -24,6 +25,13 @@ export const JsonViewerModalContainer: React.FC<JsonViewerModalProps> = ({
     <div className={styles.container}>
       <div className={styles.header}>Json viewer</div>
       <p>Function here</p>
+      <AceEditor
+        mode="json"
+        theme="github"
+        onChange={(e) => console.log(e)}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
       <div className="flex justify-end mt-3">
         <Button onClick={closeModal} className="mr-3" text={"Cancel"} />
       </div>
